28d9dd7ed5bd0fcd8f6beb18d73d384e1d63bca7
qwrtsdev <potatoloadzch@gmail.com>
2025-09-16 23:17:31 +0700

    :tada: an initial commit!

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
 import { configure } from "@ariesclark/eslint-config";
 
 export default configure({
-  stylistic: {
-    semi: true,
-    quotes: "double",
-    overrides: {
-      "style/indent": ["error", 2],
-    }
-  },
-  typescript: true,
-  rules: {
-    "jsonc/indent": ["error", 2],
-    "jsonc/sort-keys": "off",
-  }
+    stylistic: {
+        semi: true,
+        quotes: "double",
+        overrides: {
+            "style/indent": ["error", 4],
+        },
+    },
+    typescript: true,
+    rules: {
+        "jsonc/indent": ["error", 4],
+        "jsonc/sort-keys": "off",
+    },
 });
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-import { LevelDBProvider, Serenity } from "@serenityjs/core";
+import { LevelDBProvider, Serenity, ServerEvent, WorldEvent } from "@serenityjs/core";
 import { Pipeline } from "@serenityjs/plugins";
 
-// Create a new Serenity instance
 const serenity = new Serenity({
-  path: "./properties.json",
-  serenity: {
-    resourcePacks: "./resource_packs",
-    permissions: "./permissions.json",
-  },
+    path: "./properties.json",
+    serenity: {
+        resources: "./resource_packs",
+        permissions: "./permissions.json",
+    },
 });
 
-// Create a new plugin pipeline
 const _pipeline = new Pipeline(serenity, { path: "./plugins" });
-
-// Register the LevelDBProvider
 serenity.registerProvider(LevelDBProvider, { path: "./worlds" });
 
-// Start the server
+_pipeline.start();
 serenity.start();
